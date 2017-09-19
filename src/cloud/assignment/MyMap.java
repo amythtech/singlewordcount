2ff3595ec6940fb4039713c4d241609241ec2727
Amit Kumar <amyth.tech@gmail.com>
2017-09-19 00:32:26 -0400

    Split regex added

--- src/cloud/assignment/MyMap.java
+++ src/cloud/assignment/MyMap.java
@@ -10,13 +10,23 @@ public class MyMap extends Mapper<LongWritable, Text, Text, IntWritable> {
 
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		String inputStr = value.toString();
-		inputStr = inputStr.replace(".", " ");
-		String[] arrWords = inputStr.split(" ");
-		for (String eachWord : arrWords) {
-			Text outputKey = new Text(eachWord.trim());
-			IntWritable outputValue = new IntWritable(1);
-			context.write(outputKey, outputValue);
+		//inputStr = inputStr.replace(".", " ");
+		String[] lines = inputStr.split("\\r?\\n");
+		for (int i = 0; i < lines.length; i++) {
+			// divide input string by line
+			String filterLine = new String(lines[i].trim());
+
+			// Get word from each line and combine two consequetive words
+			String[] arrWords = filterLine.split(" ");
+			for (String eachWord : arrWords) {
+				if(!eachWord.trim().isEmpty()){
+					Text outputKey = new Text(eachWord.trim());
+					IntWritable outputValue = new IntWritable(1);
+					context.write(outputKey, outputValue);
+				}
+			}
 		}
+		
 	}
 
 }
